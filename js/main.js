7bd9e5c3c8a13ed11b2c56d75c502bfd672d1800
sildatema <sildatema04@gmail.com>
2024-04-04 15:04:06 +0200

    html main

--- js/main.js
+++ js/main.js
@@ -3,21 +3,37 @@ window.addEventListener('load', init);
 let urls = []
 let names = []
 let id = []
-
-
+let Description
+let tabUrl
 const Webservice = "http://localhost/CLE3/webservice/"
+let button
 
 function init(){
-    checktab()
+    Description =  document.getElementById("buttonDescription");
+    button = document.getElementById("buttonLocation")
     ajaxRequest(Webservice)
 }
 
 
 function checktab(){
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        let tabUrl = tabs[0].url;
-        console.log(tabUrl);
+        tabUrl = tabs[0].url;
+        checkIfExists()
     });
+}
+
+
+function goToPage(){
+    for (let i = 0; i < id.length; i++) {
+        let contains = names[i];
+        let included = tabUrl.includes(contains)
+        if(included){
+            console.log(urls[i])
+            chrome.tabs.create({
+                url: `${urls[i]}`
+            });
+        }
+    }
 }
 
 
@@ -30,10 +46,30 @@ function SuccesHandeler(data){
         names.push(result.name)
         id.push(result.id)
     }
-    console.log(id)
-    console.log(names)
-    console.log(urls)
+    checktab()
 }
+
+function checkIfExists(){
+    Description.innerHTML = "Er is geen toegankenlijkheids pagina gevonden voor deze website"
+    for(let i = 0; i < id.length; i++) {
+        let contains = names[i];
+        let included = tabUrl.includes(contains)
+        if(included === true){
+            Description.innerHTML = "Voor deze website is een toegankenlijkheids pagina gevonden!" +
+                " Klik op de knop om verder te gaan";
+
+            console.log("gelukt")
+
+            const buttonToPage = document.createElement('button');
+            buttonToPage.id = "buttonToPage"
+            buttonToPage.innerHTML = "Ga naar pagina";
+            buttonToPage.addEventListener('click', goToPage);
+
+            button.appendChild(buttonToPage);
+        }
+    }
+}
+
 
 function ajaxRequest(url){
     fetch(url)
@@ -48,6 +84,6 @@ function ajaxRequest(url){
 }
 
 
-function ErrorHandeler(data){
-console.error(data);
+function ErrorHandeler(data) {
+    console.error(data);
 }
